9ebde540e3e9bb3436ddff902cd7298ade873972
ecamenen <etienne.camenen@gmail.com>
2020-09-01 17:55:35 +0200

    added edam, conda tool version, 4 spaces indent

--- R/load_connection.R
+++
@@ -1,9 +0,0 @@
-load_connection <- function(file = NULL, separator = "\t"){
-    if (!is.null(file))
-        load_file(
-            file = file,
-            separator = separator,
-            rownames = NULL,
-            header = FALSE
-        )
-}
